6758d114e8dd52300bb856bea0577df5d7270be3
Ahmad Naimi <naimi.airsh@gmail.com>
2023-12-24 13:44:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [fajr, setFajr] = useState('');
+  const [dhuhr, setDhuhr] = useState('');
+  const [asr, setAsr] = useState('');
+  const [maghrib, setMaghrib] = useState('');
+  const [isha, setIsha] = useState('');
+  const [isPrayersSaved, setIsPrayersSaved] = useState(false);
+
+  const handleSavePrayers = () => {
+    // Create the request body
+    const prayerData = {
+      fajr,
+      dhuhr,
+      asr,
+      maghrib,
+      isha,
+    };
+
+    // Make the POST request to your server
+    fetch('https://sparkling-jade-cowboy-boots.cyclic.app/savePrayers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(prayerData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Prayer data saved successfully
+          console.log('Prayer data saved!');
+          // Reset the form
+          setFajr('');
+          setDhuhr('');
+          setAsr('');
+          setMaghrib('');
+          setIsha('');
+          setIsPrayersSaved(true);
+        } else {
+          console.log('Failed to save prayer data.');
+        }
+      })
+      .catch((error) => {
+        console.log('Error occurred while saving prayer data:', error);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="scrollContainer">
+        <div className="container">
+          <div className="start">
+            بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
+          </div>
+
+          <div className="prayerTitle">Add In the Prayer Times Below:</div>
+
+          <form>
+            <input
+              placeholder="Fajr"
+              value={fajr}
+              onChange={(e) => setFajr(e.target.value)}
+              className="prayerInput"
+            />
+            <input
+              placeholder="Dhuhr"
+              value={dhuhr}
+              onChange={(e) => setDhuhr(e.target.value)}
+              className="prayerInput"
+            />
+            <input
+              placeholder="Asr"
+              value={asr}
+              onChange={(e) => setAsr(e.target.value)}
+              className="prayerInput"
+            />
+            <input
+              placeholder="Maghrib"
+              value={maghrib}
+              onChange={(e) => setMaghrib(e.target.value)}
+              className="prayerInput"
+            />
+            <input
+              placeholder="Isha"
+              value={isha}
+              onChange={(e) => setIsha(e.target.value)}
+              className="prayerInput"
+            />
+
+            <button className="button" onClick={handleSavePrayers}>
+              Save Prayers
+            </button>
+          </form>
+
+          {isPrayersSaved ? (
+            <div className="confirmationContainer">
+              <div className="confirmationText">Prayers Saved!</div>
+              <button className="button2" onClick={() => setIsPrayersSaved(false)}>
+                Close
+              </button>
+            </div>
+          ) : null}
+        </div>
+      </div>
     </div>
   );
 }
