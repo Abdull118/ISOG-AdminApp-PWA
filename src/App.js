86eea8b893d762fd9b6655e471ef4c8d7786e507
Ahmad Naimi <naimi.airsh@gmail.com>
2023-12-30 20:47:29 -0500

    updated to include announcements

--- src/App.js
+++ src/App.js
@@ -1,105 +1,27 @@
 import './App.css';
 import { useState } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
 
 function App() {
-  const [fajr, setFajr] = useState('');
-  const [dhuhr, setDhuhr] = useState('');
-  const [asr, setAsr] = useState('');
-  const [maghrib, setMaghrib] = useState('');
-  const [isha, setIsha] = useState('');
-  const [isPrayersSaved, setIsPrayersSaved] = useState(false);
 
-  const handleSavePrayers = (e) => {
-    e.preventDefault();
-    // Create the request body
-    const prayerData = {
-      fajr,
-      dhuhr,
-      asr,
-      maghrib,
-      isha,
-    };
-  
-    // Make the POST request to your server using Axios
-    axios.post('https://sparkling-jade-cowboy-boots.cyclic.app/savePrayers', prayerData)
-      .then((response) => {
-        if (response.status === 200) {
-          // Prayer data saved successfully
-          console.log('Prayer data saved!');
-          // Reset the form
-          setFajr('');
-          setDhuhr('');
-          setAsr('');
-          setMaghrib('');
-          setIsha('');
-          setIsPrayersSaved(true);
-        } else {
-          console.log('Failed to save prayer data.');
-        }
-      })
-      .catch((error) => {
-        console.log('Error occurred while saving prayer data:', error);
-      });
-  };
   return (
-    <div className="container">
-      <div className="scrollContainer">
-        <div className="container">
-          <div className="start">
-            بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
-          </div>
-
-          <div className="prayerTitle">Add In the Prayer Times Below:</div>
-
-          <form>
-            <input
-              placeholder="Fajr"
-              value={fajr}
-              onChange={(e) => setFajr(e.target.value)}
-              className="prayerInput"
-            />
-            <input
-              placeholder="Dhuhr"
-              value={dhuhr}
-              onChange={(e) => setDhuhr(e.target.value)}
-              className="prayerInput"
-            />
-            <input
-              placeholder="Asr"
-              value={asr}
-              onChange={(e) => setAsr(e.target.value)}
-              className="prayerInput"
-            />
-            <input
-              placeholder="Maghrib"
-              value={maghrib}
-              onChange={(e) => setMaghrib(e.target.value)}
-              className="prayerInput"
-            />
-            <input
-              placeholder="Isha"
-              value={isha}
-              onChange={(e) => setIsha(e.target.value)}
-              className="prayerInput"
-            />
-
-            <button className="button" onClick={handleSavePrayers}>
-              Save Prayers
-            </button>
-          </form>
-
-          {isPrayersSaved ? (
-            <div className="confirmationContainer">
-              <div className="confirmationText">Prayers Saved!</div>
-              <button className="button2" onClick={() => setIsPrayersSaved(false)}>
-                Close
-              </button>
-            </div>
-          ) : null}
-        </div>
+    <>
+      <div className='tabs'>
+        <a href='/'><button>Prayer Tab</button></a>
+        <a href='/admin'><button>Announcement Tab</button></a>
       </div>
-    </div>
+    <Router>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/admin' element={<Admin />}/>
+        </Routes>
+      </Router>
+
+      
+      </>
   );
 }
 
